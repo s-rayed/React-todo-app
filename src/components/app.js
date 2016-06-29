c5051ef8e128cb6d70692aeef6ecfe24237ed14c
Rayed <siddiqui.rayed@gmail.com>
2016-06-29 16:44:27 -0400

    added delete button functionality

--- src/components/app.js
+++ src/components/app.js
@@ -32,6 +32,7 @@ export default class App extends React.Component {
           todos={this.state.todos}
           toggleTask={this.toggleTask.bind(this)}
           saveTask={this.saveTask.bind(this)}
+          deleteTask={this.deleteTask.bind(this)}
         />
       </div>
     );
@@ -54,6 +55,11 @@ export default class App extends React.Component {
   saveTask(oldTask, newTask) {
     const foundTodo = _.find(this.state.todos, todo => todo.task === oldTask);
     foundTodo.task = newTask;
+    this.setState({ todos: this.state.todos });
+  }
+
+  deleteTask(taskToDelete) {
+    _.remove(this.state.todos, todo => todo.task === taskToDelete);
     this.setState({ todos: this.state.todos });
   }
 
